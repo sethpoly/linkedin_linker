aa8be30d0a155f3194a44e2efb5eeb231f09367c
sethpoly <emailspolyniak@gmail.com>
2021-05-19 11:22:40 -0400

    Added comments

--- app.py
+++ app.py
@@ -4,21 +4,14 @@ from bs4 import BeautifulSoup
 # > Get the 'Copy Link' from LinkedIn job posting in /Jobs
 # > Retrieve the first href from the page.content to get link to job posting
 
-URL = 'https://www.linkedin.com/jobs/view/2554218127'
+URL = 'https://www.linkedin.com/jobs/view/2554218127'  # Link to linkedin job posting
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
 
+# Set up beautiful soup object
+soup = BeautifulSoup(page.content, 'html.parser')
 result = soup.find(id="main-content")
 print(result.prettify())
-
-# Find parent class for company name and location
-# company_and_location = result.find('p',
-#                                    class_='unify-apply-page__company-name-location')
-#
-# # Retrieve the company name and location
-# company_name = company_and_location.select('p > span')[0].get_text(strip=True)
-# location = company_and_location.select('p > span')[1].get_text(strip=True)
 
 # Get parent element containing comapny name and location
 company_and_location = result.find('div', class_='sub-nav-cta__sub-text-container')
@@ -27,6 +20,7 @@ company_and_location = result.find('div', class_='sub-nav-cta__sub-text-containe
 company = company_and_location.find('a', class_='sub-nav-cta__optional-url')
 location = company_and_location.select('span')[0].get_text(strip=True)
 
+# Print parsed data
 print(f'Company : {company.text}')
 print(f'Location : {location}')
 
