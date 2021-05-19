3a6e623d5de04821e60efa4c675b8b1a690c3f9b
sethpoly <emailspolyniak@gmail.com>
2021-05-19 11:06:50 -0400

    Parsed linkedin job for company name and location

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.linkedin.com/jobs/view/software-engineer-remote-at-groupgreeting-2412543282/'
+# > Get the 'Copy Link' from LinkedIn job posting in /Jobs
+# > Retrieve the first href from the page.content to get link to job posting
+
+URL = 'https://www.linkedin.com/jobs/view/2507865886/'
 page = requests.get(URL)
 
-# Get the 'Copy Link' from LinkedIn job posting in /Jobs
+soup = BeautifulSoup(page.content, 'html.parser')
 
-# Retrieve the first href from the page.content to get link to job posting
+result = soup.find(id="main-content")
+print(result.prettify())
+
+# Find parent class for company name and location
+company_and_location = result.find('p',
+                                   class_='unify-apply-page__company-name-location')
+
+# Retrieve the company name and location
+company_name = company_and_location.select('p > span')[0].get_text(strip=True)
+location = company_and_location.select('p > span')[1].get_text(strip=True)
+
+print(f'Company: {company_name}')
+print(f'Location: {location}')
 
 # Job Title : unify-apply-page__job-title
 # Company Name : unify-apply-page__company-name-location
-print(page.content)
-
